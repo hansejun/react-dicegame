3f6b5c2f75984fb173299f7ad68d964125336c9a
hansejun <tpwnszja2@gmail.com>
2022-02-17 20:31:57 +0900

    Learning React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Dice from "./Dice"
+import Button from "./Button";
 function App(){
   return (
     <div>
-      <Dice/>
+      <div>
+        <Button>처음부터</Button>
+        <Button>다시하기</Button>
+      </div>
+      <Dice color="red" num={4}/>
     </div>
   )
 }
